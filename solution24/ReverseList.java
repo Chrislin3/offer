b72081790a300ff6cb49828b56e78a16afbce884
tessss <qhlin_sdust@163.com>
2019-04-26 23:28:01 +0800

    剑指offer

---
+++ solution24/ReverseList.java
@@ -0,0 +1,21 @@
+package solution24;
+/**
+ * 输入一个链表，反转链表后，输出新链表的表头。
+ */
+
+import solution6.ListNode;
+
+public class ReverseList {
+    public ListNode reverseList(ListNode head){
+        ListNode pre = null;
+        ListNode cur = head;
+
+        while (cur != null){
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
---
+++ solution26/HasSubtree.java
@@ -0,0 +1,12 @@
+package solution26;
+
+
+public class HasSubtree {
+    public boolean hasSubtree(TreeNode root1, TreeNode root2) {
+        if(root1 == null || root2 == null){
+            return false;
+        }
+
+
+    }
+}
